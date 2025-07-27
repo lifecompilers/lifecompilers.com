92c15ea1caea6c30651d72a49b817222cb4e7780
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2025-07-27 13:38:35 +0530

    Fix images and create homepage

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,20 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
     unoptimized: true,
   },
 }
